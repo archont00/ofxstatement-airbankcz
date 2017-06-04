3cf1a485d9b3fef2b6d83f6aa1d587c8d5ca0314
Milan Knížek <milankni.git@gmail.com>
2017-06-04 22:32:44 +0200

    Added shell scripts to handle -fees.csv semi-automatically

---
+++ tools/mk_bank-air-CA.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# This is a wrapper for
+#   ofxstatement convert -t airbankcz input.csv output.ofx
+# which:
+# - if there is any input-fees.csv, it runs again
+# - reformats output.ofx to be human readable
+
+# ToDo:
+# - merge input-fees.ofx with output.ofx
+
+# Dependency:
+# - bash
+# - ofxstatement
+# - ofxstatement-airbankcz (plugin)
+# - uuidgen
+# - xmllint
+
+if [[ "x$1" = "x" ]]; then
+  echo "Expects bank statement history CSV file as input."
+  echo "Usage:"
+  echo "  $0 input.csv"
+  exit 1
+fi
+
+if [[ ! -f "${1}" ]]; then
+  echo "File ${1} does not exist or is not readable"
+  exit 1
+fi
+
+inputf="$(basename ${1} .csv)"
+inputd="$(dirname ${1})"
+
+# Run ofxstatement 
+ofxstatement convert -t airbankcz:CA "${1}" "${inputd}/${inputf}.ofx" \
+  || { echo "ofxstatement 1 failed."; exit 1; }
+
+
+tmpf="$(uuidgen)"
+cat "${inputd}/${inputf}.ofx" | xmllint --format - > "${inputd}/${tmpf}"
+mv "${inputd}/${tmpf}" "${inputd}/${inputf}.ofx"
+
+feesf="${inputd}/${inputf}-fees.csv"
+if [[ -f "${feesf}" ]]; then
+  if [[ $(cat "${feesf}" | wc -l) -gt 1 ]]; then
+    ofxstatement convert -t airbankcz:CA "${feesf}" "${feesf}.ofx" \
+      || { echo "ofxstatement 2 failed."; exit 1; }
+  fi
+  rm "${feesf}"
+fi
